25c2264f901e114ad6bd667d87684b738efce8f1
Tim Riffe <tim.riffe@gmail.com>
2016-03-23 13:40:11 +0100

    added TOL argument, HT RR

--- Lotka/R/LotkaRoptim.R
+++ Lotka/R/LotkaRoptim.R
@@ -1,9 +1,9 @@
 LotkaRoptim <-
-function(fx,Lx,x){
+function(fx,Lx,x=1:length(fx)-.5,TOL=.Machine$double.eps){
 	findR <- function(r){
 		# this is the Lotka equation, with the 1 moved over to the right side!
 		(1-sum(exp(-r*x)*fx*Lx))^2
 	}
-	optimize(f=findR,seq(-1,1,.0001),tol=.00000000001)$minimum
+	optimize(f=findR,seq(-1,1,.0001),tol=TOL)$minimum
 }
 
